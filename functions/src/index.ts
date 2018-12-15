39307a1e9b4e5e9f556765fb5ad8f98f00c16233
dpett122 <dpett122@gmail.com>
2018-12-15 13:09:24 -0500

    fixing push notifs for deployment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -220,7 +220,7 @@ exports.newWallet = functions.database.ref('/users/{uid}/wallets/{walletId}').on
   // curl -sd '{"event": "confirmed-tx", "address": "14ddda446688b73161aa1382f4e4343353af6fc8", "url": "https://webhook.site/ccb360a4-7409-4e82-8820-f61f3b0ac3cd"}' https://api.blockcypher.com/v1/eth/main/hooks?token=117cfc5e59ea48b9a0fadb9a24ba4702
 
   const blockcypherData = JSON.stringify({
-    url: webhookCallbackUrl + `&walletAddress=${walletAddress}&walletId=${walletId}&walletNickname=${walletNickname.split(' ').join('_')}&uid=${uid}`,
+    url: webhookCallbackUrl + `&walletAddress=${walletAddress}&walletId=${walletId}&walletNickname=${walletNickname.split(' ').join('_')}&uid=${uid}`, // 'https://webhook.site/8935ec96-b32b-425a-b8d8-2c0db133966d', 
     'event': 'confirmed-tx',
     'address': (walletAddress[0] === '0' && walletAddress[1] === 'x') ? walletAddress.substr(2) : walletAddress,
     'token': BLOCKCYPHER_API_KEY
